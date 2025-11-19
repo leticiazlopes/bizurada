c49a1f6a3c6f6805211c112b70053b4b2bd6ea20
Leticia Lopes Lima <leticiazlzlima@gmail.com>
2025-11-19 16:14:59 -0300

    add spring

---
+++ bizurada/src/main/java/br/edu/ifpb/pweb2/bizurada/BizuradaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.pweb2.bizurada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BizuradaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BizuradaApplication.class, args);
+	}
+
+}
---
+++ bizurada/src/test/java/br/edu/ifpb/pweb2/bizurada/BizuradaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.pweb2.bizurada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BizuradaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
