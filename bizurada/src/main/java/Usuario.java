f4673c8a4215dedb40a40573fb905911f16b1d5a
Laila Aquino <lailaaquino8@gmail.com>
2025-11-19 16:24:11 -0300

    adiciona classe

---
+++ bizurada/src/main/java/Usuario.java
@@ -0,0 +1,18 @@
+//package br.edu.ifpb.pweb2.biruzada;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Usuario implements Serializable {
+    private String id;
+	private String nome;
+
+
+}
