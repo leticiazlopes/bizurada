968250a109299b71469426718d70e37a39ef4155
Luan Pimenta Fernandes <130945489+pimentaluan@users.noreply.github.com>
2025-11-19 16:48:33 -0300

    Add files via upload

---
+++ bizurada/src/main/java/br/edu/ifpb/pweb2/bizurada/controller/AuthController.java
@@ -0,0 +1,58 @@
+package br.edu.ifpb.pweb2.bizurada.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import br.edu.ifpb.pweb2.bizurada.model.Usuario;
+import br.edu.ifpb.pweb2.bizurada.repository.UsuarioRepository;
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private UsuarioRepository usuarioRepo;
+
+    @GetMapping
+    public ModelAndView getForm(ModelAndView model) {
+        model.setViewName("auth/login");
+        model.addObject("usuario", new Usuario());
+        return model;
+    }
+
+    @PostMapping
+    public ModelAndView valide(Usuario usuario, HttpSession session, ModelAndView model,
+            RedirectAttributes redirectAttts) {
+        if ((usuario = this.isValido(usuario)) != null) {
+            session.setAttribute("usuario", usuario);
+            model.setViewName("redirect:/home");
+        } else {
+            redirectAttts.addFlashAttribute("mensagem", "Login inválidos!");
+            model.setViewName("redirect:/auth");
+        }
+        return model;
+    }
+
+    @GetMapping("/logout")
+    public ModelAndView logout(ModelAndView mav, HttpSession session) {
+        session.invalidate();
+        mav.setViewName("redirect:/auth");
+        return mav;
+    }
+
+    private Usuario isValido(Usuario usuario) {
+        Usuario usuarioEncontrado = usuarioRepo.findByName(usuario.getNome());
+        if (usuarioEncontrado != null) {
+            return usuarioEncontrado;
+        }
+        return null;
+    }
+
+}
+
---
+++ bizurada/src/main/java/br/edu/ifpb/pweb2/bizurada/repository/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package br.edu.ifpb.pweb2.bizurada.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.edu.ifpb.pweb2.bizurada.model.Usuario;
+
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, String> {
+    Usuario findByName(String name);
+}
